fe415f6a05958b4797b069457ec3002075e9779b
agusgatto8 <agustingatto8@gmail.com>
2022-09-15 19:14:12 -0300

    BaSP-W02-2022: I do exercise one and two

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,21 @@
+// SECTION A//
+/*
+var num1 = 6;
+var num2 = 14;
+var result = num1 + num2;
+console.log(result)
+
+
+// SECTION B//
+
+var name1 = "My name is Agustin, "
+var lastName = "my last name is Gatto"
+var myString = name1 + lastName;
+console.log(myString)
+
+//SECTION C//
+
+var myVar1 = "Hello";
+var myVar2 = "Agustin";
+var result2 = myVar1.length + myVar2.length;
+console.log("The length of both strings is: " + result2)*/
---
+++ Semana-05/exercises/strings.js
@@ -0,0 +1,42 @@
+// SECTION A//
+
+/*var myVarUp = "This is a string";
+myVarUp = myVarUp.toUpperCase();
+console.log("Exercise 2-A: " + myVarUp)
+
+// SECTION B//
+
+var randomVar = "abcdefghij";
+var subRandomVar = randomVar.substring(0,5);
+console.log("Excercise 2-B: " + subRandomVar);
+
+//SECTION C//
+
+var sectionC = "abcdefghij";
+var subSectionC = sectionC.substring(6,9);
+console.log("Exercise 2-C: " + subSectionC);
+
+// SECTION D //
+
+var sectionD = "abcdefghij";
+var subSectionD = sectionD.substring(0,1).toUpperCase(0);
+var subStringD = sectionD.substring(1,9).toLowerCase();
+console.log("Exercise 2-D: " + subSectionD + subStringD);
+
+// SECTION E//
+
+var spaceFree = "abcde fghij";
+var spaceFreeE = spaceFree.indexOf(" ");
+console.log("Exercise 2-E: " + spaceFreeE);
+*/
+
+// SECTION F//
+
+var sectionF = "agustin gatto";
+var subSectionF = sectionF.substring(0,1).toUpperCase(0);
+var subStringF = sectionF.substring(1,8).toLowerCase();
+
+var sectionR = sectionF.substring(8,9).toUpperCase(8);
+var subStringR = sectionF.substring(9,13).toLowerCase();
+var space = sectionF.indexOf(" ");
+console.log("Exercise 2-F: " + subSectionF + subStringF + sectionR + subStringR + " the index of the space is: " + space);
