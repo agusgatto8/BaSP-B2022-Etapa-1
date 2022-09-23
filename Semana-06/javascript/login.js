7cd25e659a2bad38d1613bfae4b923fc73933361
agusgatto8 <agustingatto8@gmail.com>
2022-09-23 10:48:51 -0300

    BaSP-W06-2022: Start with sign in screen.

---
+++ Semana-06/javascript/login.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+
+var tituloUno = document.getElementById("h1");
+tituloUno.textContent = "Hola esta es mi primer interaccion con el DOM";
+
+
+var tituloUno = document.getElementById("h1");
+tituloUno.className = "peligro";
+
+
+
+
+
+
+
+
+
+
+
+
+};
