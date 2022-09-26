474166539aae26923e61f1249ec16b38a28b7456
agusgatto8 <agustingatto8@gmail.com>
2022-09-26 01:06:46 -0300

    BaSP-W06-2022: Advance with sign up validations.

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -35,4 +35,110 @@ lastName.onblur = function(){
 lastName.onfocus = function(){
     lastName.classList.remove("red-border");
 };
+
+//VALIDATE DNI
+
+var inputDni = document.getElementById("input-dni");
+inputDni.onblur = function(){
+    var expValDni = (/^[0-9]{7,20}$/);
+    var dniValidate = expValDni.test(inputDni.value);
+    if (dniValidate == true){
+        console.log("Dni approval");
+        inputDni.classList.add("border-color");
+    } else{
+        console.log("Dni failed");
+        inputDni.classList.add("border-red");
+    }
+};
+inputDni.onfocus = function(){
+    inputDni.classList.remove("red-border");
+};
+
+//Validate Number
+
+var inputPhone = document.getElementById("input-phone");
+inputPhone.onblur = function(){
+    var expValPhone = (/^[0-9]{10,11}$/);
+    var phoneValidate = expValPhone.test(inputPhone.value);
+    if (phoneValidate == true){
+        console.log("Phone approval");
+        inputPhone.classList.add("border-color");
+    } else{
+        console.log("Phone failed");
+        inputPhone.classList.add("border-red");
+    }
+};
+inputPhone.onfocus = function(){
+    inputPhone.classList.remove("red-border");
+};
+
+//Validate Adress
+
+var inputAdress = document.getElementById("input-adress");
+inputAdress.onblur = function(){
+    var expValAdress = (/^["a-zA-Z"0-9]{5,20}$/);
+    var adressValidate = expValAdress.test(inputAdress.value);
+    if (adressValidate == true){
+        console.log("adress approval");
+        inputAdress.classList.add("border-color");
+    } else{
+        console.log("adress failed");
+        inputAdress.classList.add("border-red");
+    }
+};
+inputAdress.onfocus = function(){
+    inputAdress.classList.remove("red-border");
+};
+
+// Validate Location
+
+var inputLocation = document.getElementById("input-location");
+inputLocation.onblur = function(){
+    var expValLocation = (/^[A-Z]{3,20}$/i);
+    var locationValidate = expValLocation.test(inputLocation.value);
+    if (locationValidate === true){
+        console.log("location approval");
+        inputLocation.classList.add("border-color");
+    } else{
+        console.log("location failed");
+        inputLocation.classList.add("border-red");
+    }
+};
+inputLocation.onfocus = function(){
+    inputLocation.classList.remove("red-border");
+};
+
+// Validate Postal Code
+
+var inputPostal = document.getElementById("input-postal");
+inputPostal.onblur = function(){
+    var expValPostal = (/^[0-9]{4,5}$/);
+    var postalValidate = expValPostal.test(inputPostal.value);
+    if (postalValidate == true){
+        console.log("Postal code approval");
+        inputPostal.classList.add("border-color");
+    } else{
+        console.log("Postal code failed");
+        inputPostal.classList.add("border-red");
+    }
+};
+inputPostal.onfocus = function(){
+    inputPostal.classList.remove("red-border");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
