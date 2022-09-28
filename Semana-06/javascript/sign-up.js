c46fa195d77c0c33cc871fe3032d0103e1e2c805
agusgatto8 <agustingatto8@gmail.com>
2022-09-27 23:46:28 -0300

    BaSP-W06-2022: Finish.

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,8 +1,16 @@
 window.onload = function(){
+    var expVal = (/^[a-zA-Z]{3,20}$/);
+    var expValTwo = (/^[a-zA-Z]{3,20}$/);
+    var expValDni = (/^[0-9]{7,20}$/);
+    var expValPhone = (/^[0-9]{10,11}$/);
+    var expValAdress = (/^['a-zA-Z'0-9 ]{5,20}$/);
+    var expValLocation = (/^[A-Z]{3,20}$/i);
+    var expValPostal = (/^[0-9]{4,5}$/);
+    var expValEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var expPassword = /^['a-z'0-9]{8,20}$/;
 
-var firsName = document.getElementById('first-name');
+    var firsName = document.getElementById('first-name');
     firsName.onblur = function(){
-        var expVal = (/^[a-zA-Z]{3,20}$/);
         var nameValidate = expVal.test(firsName.value);
         if (nameValidate == true){
             firsName.classList.add('border-color');
@@ -19,7 +27,6 @@ var firsName = document.getElementById('first-name');
 
     var lastName = document.getElementById('last-name');
     lastName.onblur = function(){
-        var expValTwo = (/^[a-zA-Z]{3,20}$/);
         var lastNameValidate = expValTwo.test(lastName.value);
         if (lastNameValidate == true){
             lastName.classList.add('border-color');
@@ -36,7 +43,6 @@ var firsName = document.getElementById('first-name');
 
     var inputDni = document.getElementById('input-dni');
     inputDni.onblur = function(){
-        var expValDni = (/^[0-9]{7,20}$/);
         var dniValidate = expValDni.test(inputDni.value);
         if (dniValidate == true){
             inputDni.classList.add('border-color');
@@ -53,7 +59,6 @@ var firsName = document.getElementById('first-name');
 
     var inputPhone = document.getElementById('input-phone');
     inputPhone.onblur = function(){
-        var expValPhone = (/^[0-9]{10,11}$/);
         var phoneValidate = expValPhone.test(inputPhone.value);
         if (phoneValidate == true){
             inputPhone.classList.add('border-color');
@@ -70,7 +75,6 @@ var firsName = document.getElementById('first-name');
 
     var inputAdress = document.getElementById('input-adress');
     inputAdress.onblur = function(){
-        var expValAdress = (/^['a-zA-Z'0-9 ]{5,20}$/);
         var adressValidate = expValAdress.test(inputAdress.value);
         if (adressValidate == true){
             inputAdress.classList.add('border-color');
@@ -87,7 +91,6 @@ var firsName = document.getElementById('first-name');
 
     var inputLocation = document.getElementById('input-location');
     inputLocation.onblur = function(){
-        var expValLocation = (/^[A-Z]{3,20}$/i);
         var locationValidate = expValLocation.test(inputLocation.value);
         if (locationValidate === true){
             inputLocation.classList.add('border-color');
@@ -104,7 +107,6 @@ var firsName = document.getElementById('first-name');
 
     var inputPostal = document.getElementById('input-postal');
     inputPostal.onblur = function(){
-        var expValPostal = (/^[0-9]{4,5}$/);
         var postalValidate = expValPostal.test(inputPostal.value);
         if (postalValidate == true){
             inputPostal.classList.add('border-color');
@@ -122,14 +124,13 @@ var firsName = document.getElementById('first-name');
 
     var inputEmail = document.getElementById('input-email');
         inputEmail.onblur = function(){
-        var expValEmail =  (/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/);
         var emailValidate = expValEmail.test(inputEmail.value);
         if (emailValidate == true){
             inputEmail.classList.add('border-color');
-        } else if(inputEmail.value = ''){
-            inputEmail.classList.add('border-red');
+            inputEmail.classList.remove('border-red');
         } else{
             inputEmail.classList.add('border-red');
+            inputEmail.classList.remove('border-color');
         };
     };
     inputEmail.onfocus = function(){
@@ -138,7 +139,6 @@ var firsName = document.getElementById('first-name');
 
     var inputPassword = document.getElementById('input-password');
     inputPassword.onblur = function(){
-            expPassword = /^['a-z'0-9]{8,20}$/;
             var validatePassword1 = expPassword.test(inputPassword.value);
             if (validatePassword1 == true){
                 inputPassword.classList.add('border-color')
