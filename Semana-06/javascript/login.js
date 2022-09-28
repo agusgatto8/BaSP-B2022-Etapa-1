6ba908bed45d9a41fe5c9b5cf4722106a8b8810d
agusgatto8 <agustingatto8@gmail.com>
2022-09-27 23:07:39 -0300

    BaSP-W06-2022: Add changes login.js.

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -1,40 +1,52 @@
 window.onload = function() {
+    var expRegPassword = /^['a-zA-Z'0-9]{8,20}$/;
+    var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
-    var emailEnter = document.getElementById('emailEnter');
+    var emailEnter = document.getElementById('email-enter');
     emailEnter.onblur = function(){
-        var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
         var validateEmail = expReg.test(emailEnter.value);
         if (validateEmail == true){
-            console.log(emailEnter.value);
-        } else{
-            emailEnter.classList.add("red-border");
+            emailEnter.classList.remove('red-border')
+            emailEnter.classList.add('green-border');
+        }  else{
+            emailEnter.classList.add('red-border');
+            emailEnter.classList.remove('green-border');
         };
-
-    };
-        emailEnter.onfocus = function(){
-            emailEnter.classList.remove("red-border");
-    };
-    var passwordEnter = document.getElementById('passwordEnter');
-    passwordEnter.onblur = function(){
-        expRegPassword = /^["a-zA-Z"0-9]{8,20}$/;
-        var validatePassword = expRegPassword.test(passwordEnter.value);
-        if (validatePassword == true){
-            console.log("password correct");
-        } else{
-            passwordEnter.classList.add("red-border")
+    };      
+    emailEnter.onfocus = function(){
+        emailEnter.classList.remove('red-border');
+    };     
+    var passwordEnter = document.getElementById('password-enter');
+        passwordEnter.onblur = function(){
+            var validatePassword = expRegPassword.test(passwordEnter.value);
+            if (validatePassword == true){
+                passwordEnter.classList.remove('red-border')
+                passwordEnter.classList.add('green-border');
+            } else{
+                passwordEnter.classList.add('red-border')
+                passwordEnter.classList.remove('green-border');
+            };
         };
-    };
     passwordEnter.onfocus = function(){
-        passwordEnter.classList.remove("red-border");
-}; 
-    
+        passwordEnter.classList.remove('red-border');
+    };
 
-    var buttonForm = document.getElementById('button-form');
-    buttonForm.click = function(a){
-        a.preventDefault();
-        console.log(emailEnter.value);
-        } 
-    }
+  var buttonForm = document.getElementById('button-form');
+   buttonForm.onclick = function(e){
+        e.preventDefault();
+       if (expReg.test(emailEnter.value) && expRegPassword.test(passwordEnter.value)){
+           alert('Email: ' + emailEnter.value + "\n" + 'Password: ' + passwordEnter.value);
+       } else if(!expReg.test(emailEnter.value) && expRegPassword.test(passwordEnter.value)){
+           alert('Email false');
+       } else if(expReg.test(emailEnter.value) && !expRegPassword.test(passwordEnter.value)){
+        alert('Password false');
+       } else{
+            alert("Email and Password false");
+       };
+    };
+};   
+
+
 
 
 
--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,223 +1,168 @@
 window.onload = function(){
 
-//Validate NAME
-
 var firsName = document.getElementById('first-name');
-firsName.onblur = function(){
-    var expVal = (/^[a-zA-Z]{3,20}$/);
-    var nameValidate = expVal.test(firsName.value);
-    if (nameValidate == true){
-        console.log('nombre correcto');
-        firsName.classList.add('border-color');
-    } else if(firsName.value = ''){
-        firsName.classList.add('border-red');
-    }else{
-        console.log('Nombre incorrecto');
-        firsName.classList.add('border-red');
-    };
-};
-firsName.onfocus = function(){
-    firsName.classList.remove('border-color');
-    firsName.classList.remove('border-red');
-};
-
-//Validate LASTNAME
-
-var lastName = document.getElementById('last-name');
-lastName.onblur = function(){
-    var expValTwo = (/^[a-zA-Z]{3,20}$/);
-    var lastNameValidate = expValTwo.test(lastName.value);
-    if (lastNameValidate == true){
-        console.log('Apellido correcto');
-        lastName.classList.add('border-color');
-    } else if(lastName.value = ''){
-        lastName.classList.add('border-red');
-        console.log('Campo requerido');
-    } else{
-       console.log('Apellido incorrecto');
-       lastName.classList.add('border-red');
-   };   
-};
-lastName.onfocus = function(){
-    lastName.classList.remove('border-color');
-    lastName.classList.remove('red-border');
-};
-
-//VALIDATE DNI
-
-var inputDni = document.getElementById('input-dni');
-inputDni.onblur = function(){
-    var expValDni = (/^[0-9]{7,20}$/);
-    var dniValidate = expValDni.test(inputDni.value);
-    if (dniValidate == true){
-        console.log('Dni approval');
-        inputDni.classList.add('border-color');
-    } else if(inputDni.value = ''){
-        inputDni.classList.add('border-red');
-        console.log('Campo requerido');
-    } else{
-        console.log('Dni failed');
-        inputDni.classList.add('border-red');
-    };
-};
-inputDni.onfocus = function(){
-    inputDni.classList.remove('border-color');
-    inputDni.classList.remove('red-border');
-};    
-
-
-//Validate Number
-
-var inputPhone = document.getElementById('input-phone');
-inputPhone.onblur = function(){
-    var expValPhone = (/^[0-9]{10,11}$/);
-    var phoneValidate = expValPhone.test(inputPhone.value);
-    if (phoneValidate == true){
-        console.log('Phone approval');
-        inputPhone.classList.add('border-color');
-    } else if(inputPhone.value = ''){
-        inputPhone.classList.add('border-red');
-        console.log('Campo requerido');
-    } else{
-        console.log('Phone failed');
-        inputPhone.classList.add('border-red');
-    };
-};
-inputPhone.onfocus = function(){
-    inputPhone.classList.remove('border-color');
-    inputPhone.classList.remove('red-border');
-};
-
-//Validate Adress
-
-var inputAdress = document.getElementById('input-adress');
-inputAdress.onblur = function(){
-    var expValAdress = (/^['a-zA-Z'0-9 ]{5,20}$/);
-    var adressValidate = expValAdress.test(inputAdress.value);
-    if (adressValidate == true){
-        console.log('adress approval');
-        inputAdress.classList.add('border-color');
-    } else if(inputAdress.value = ''){
-        inputAdress.classList.add('border-red');
-        console.log('Campo requerido');
-    } else{
-        console.log('adress failed');
-        inputAdress.classList.add('border-red');
-    };
-};
-inputAdress.onfocus = function(){
-    inputAdress.classList.remove('border-color');
-    inputAdress.classList.remove('red-border');
-};
-
-// Validate Location
-
-var inputLocation = document.getElementById('input-location');
-inputLocation.onblur = function(){
-    var expValLocation = (/^[A-Z]{3,20}$/i);
-    var locationValidate = expValLocation.test(inputLocation.value);
-    if (locationValidate === true){
-        console.log('location approval');
-        inputLocation.classList.add('border-color');
-    } else if(inputLocation.value = ''){
-        inputLocation.classList.add('border-red');
-        console.log('Campo requerido');
-    } else{
-        console.log('location failed');
-        inputLocation.classList.add('border-red');
-    };
-};
-inputLocation.onfocus = function(){
-    inputLocation.classList.remove('border-color');
-    inputLocation.classList.remove('red-border');
-};
-
-// Validate Postal Code
-
-var inputPostal = document.getElementById('input-postal');
-inputPostal.onblur = function(){
-    var expValPostal = (/^[0-9]{4,5}$/);
-    var postalValidate = expValPostal.test(inputPostal.value);
-    if (postalValidate == true){
-        console.log('Postal code approval');
-        inputPostal.classList.add('border-color');
-    } else if(inputPostal.value = ''){
-        inputPostal.classList.add('border-red');
-        console.log('Campo requerido');
-    } else{
-        console.log('Postal code failed');
-        inputPostal.classList.add('border-red');
-        inputPostal.value.classList.add('border-red');
-    };
-};
-inputPostal.onfocus = function(){
-    inputPostal.classList.remove('border-color');
-    inputPostal.classList.remove('red-border');
-};
-
-//Validate Email
-
-
-var inputEmail = document.getElementById('input-email');
-    inputEmail.onblur = function(){
-    var expValEmail =  (/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/);
-    var emailValidate = expValEmail.test(inputEmail.value);
-    if (emailValidate == true){
-        console.log('nombre correcto');
-        inputEmail.classList.add('border-color');
-    } else if(inputEmail.value = ''){
-        inputEmail.classList.add('border-red');
-    } else{
-        console.log('Nombre incorrecto');
-        inputEmail.classList.add('border-red');
-    };
-};
-inputEmail.onfocus = function(){
-    inputEmail.classList.remove('border-red');
-};
-
-//VALIDATE PASSWORD 1
-
-var inputPassword = document.getElementById('input-password');
-
-inputPassword.onblur = function(){
-        expPassword = /^['a-z'0-9]{8,20}$/;
-        var validatePassword1 = expPassword.test(inputPassword.value);
-        if (validatePassword1 == true){
-            console.log('password correct');
-            inputPassword.classList.add('border-color')
-        } else if (inputPassword.value === inputRepeat.value){
-            inputPassword.classList.add('border-color')
+    firsName.onblur = function(){
+        var expVal = (/^[a-zA-Z]{3,20}$/);
+        var nameValidate = expVal.test(firsName.value);
+        if (nameValidate == true){
+            firsName.classList.add('border-color');
+        } else if(firsName.value = ''){
+            firsName.classList.add('border-red');
         }else{
-            console.log('error');
-            inputPassword.classList.add('border-red');
+            firsName.classList.add('border-red');
         };
     };
-inputPassword.onfocus = function(){
-    inputPassword.classList.remove('border-color');
-    inputPassword.classList.remove('border-red');
-}; 
-
-// VALIDATE PASSWORD CONFIRMATION
-var inputRepeat = document.getElementById('input-repeat') ;
-inputRepeat.onblur = function(){
-    if (inputRepeat.value === inputPassword.value){
-        console.log('Coinciden');
-        inputRepeat.classList.add('border-color')
-    } else{
-        console.log('No coinciden');
-        inputRepeat.classList.add('border-red')
+    firsName.onfocus = function(){
+        firsName.classList.remove('border-color');
+        firsName.classList.remove('border-red');
     };
-};
-inputRepeat.onfocus = function(){
-    inputRepeat.classList.remove('border-color');
-    inputRepeat.classList.remove('border-red');
-}; 
 
+    var lastName = document.getElementById('last-name');
+    lastName.onblur = function(){
+        var expValTwo = (/^[a-zA-Z]{3,20}$/);
+        var lastNameValidate = expValTwo.test(lastName.value);
+        if (lastNameValidate == true){
+            lastName.classList.add('border-color');
+        } else if(lastName.value = ''){
+            lastName.classList.add('border-red');
+        } else{
+        lastName.classList.add('border-red');
+    };   
+    };
+    lastName.onfocus = function(){
+        lastName.classList.remove('border-color');
+        lastName.classList.remove('red-border');
+    };
 
+    var inputDni = document.getElementById('input-dni');
+    inputDni.onblur = function(){
+        var expValDni = (/^[0-9]{7,20}$/);
+        var dniValidate = expValDni.test(inputDni.value);
+        if (dniValidate == true){
+            inputDni.classList.add('border-color');
+        } else if(inputDni.value = ''){
+            inputDni.classList.add('border-red');
+        } else{
+            inputDni.classList.add('border-red');
+        };
+    };
+    inputDni.onfocus = function(){
+        inputDni.classList.remove('border-color');
+        inputDni.classList.remove('red-border');
+    };    
 
+    var inputPhone = document.getElementById('input-phone');
+    inputPhone.onblur = function(){
+        var expValPhone = (/^[0-9]{10,11}$/);
+        var phoneValidate = expValPhone.test(inputPhone.value);
+        if (phoneValidate == true){
+            inputPhone.classList.add('border-color');
+        } else if(inputPhone.value = ''){
+            inputPhone.classList.add('border-red');
+        } else{
+            inputPhone.classList.add('border-red');
+        };
+    };
+    inputPhone.onfocus = function(){
+        inputPhone.classList.remove('border-color');
+        inputPhone.classList.remove('red-border');
+    };
 
+    var inputAdress = document.getElementById('input-adress');
+    inputAdress.onblur = function(){
+        var expValAdress = (/^['a-zA-Z'0-9 ]{5,20}$/);
+        var adressValidate = expValAdress.test(inputAdress.value);
+        if (adressValidate == true){
+            inputAdress.classList.add('border-color');
+        } else if(inputAdress.value = ''){
+            inputAdress.classList.add('border-red');
+        } else{
+            inputAdress.classList.add('border-red');
+        };
+    };
+    inputAdress.onfocus = function(){
+        inputAdress.classList.remove('border-color');
+        inputAdress.classList.remove('red-border');
+    };
 
+    var inputLocation = document.getElementById('input-location');
+    inputLocation.onblur = function(){
+        var expValLocation = (/^[A-Z]{3,20}$/i);
+        var locationValidate = expValLocation.test(inputLocation.value);
+        if (locationValidate === true){
+            inputLocation.classList.add('border-color');
+        } else if(inputLocation.value = ''){
+            inputLocation.classList.add('border-red');
+        } else{
+            inputLocation.classList.add('border-red');
+        };
+    };
+    inputLocation.onfocus = function(){
+        inputLocation.classList.remove('border-color');
+        inputLocation.classList.remove('red-border');
+    };
 
+    var inputPostal = document.getElementById('input-postal');
+    inputPostal.onblur = function(){
+        var expValPostal = (/^[0-9]{4,5}$/);
+        var postalValidate = expValPostal.test(inputPostal.value);
+        if (postalValidate == true){
+            inputPostal.classList.add('border-color');
+        } else if(inputPostal.value = ''){
+            inputPostal.classList.add('border-red');
+        } else{
+            inputPostal.classList.add('border-red');
+            inputPostal.value.classList.add('border-red');
+        };
+    };
+    inputPostal.onfocus = function(){
+        inputPostal.classList.remove('border-color');
+        inputPostal.classList.remove('red-border');
+    };
 
+    var inputEmail = document.getElementById('input-email');
+        inputEmail.onblur = function(){
+        var expValEmail =  (/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/);
+        var emailValidate = expValEmail.test(inputEmail.value);
+        if (emailValidate == true){
+            inputEmail.classList.add('border-color');
+        } else if(inputEmail.value = ''){
+            inputEmail.classList.add('border-red');
+        } else{
+            inputEmail.classList.add('border-red');
+        };
+    };
+    inputEmail.onfocus = function(){
+        inputEmail.classList.remove('border-red');
+    };
+
+    var inputPassword = document.getElementById('input-password');
+    inputPassword.onblur = function(){
+            expPassword = /^['a-z'0-9]{8,20}$/;
+            var validatePassword1 = expPassword.test(inputPassword.value);
+            if (validatePassword1 == true){
+                inputPassword.classList.add('border-color')
+            } else if (inputPassword.value === inputRepeat.value){
+                inputPassword.classList.add('border-color')
+            }else{
+                inputPassword.classList.add('border-red');
+            };
+    };
+    inputPassword.onfocus = function(){
+        inputPassword.classList.remove('border-color');
+        inputPassword.classList.remove('border-red');
+    };
+
+    var inputRepeat = document.getElementById('input-repeat') ;
+    inputRepeat.onblur = function(){
+        if (inputRepeat.value === inputPassword.value){
+            inputRepeat.classList.add('border-color')
+        } else{
+            inputRepeat.classList.add('border-red')
+        };
+    };
+    inputRepeat.onfocus = function(){
+        inputRepeat.classList.remove('border-color');
+        inputRepeat.classList.remove('border-red');
+    };
 };
